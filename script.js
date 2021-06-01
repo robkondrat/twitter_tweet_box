6c52a6a6e40c3d0fec51e4c93f0aa94786a42778
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-06-01 16:31:00 -0500

    user can see counting characters, text over limit will highlight, counter goes neg/red

---
+++ script.js
@@ -0,0 +1,40 @@
+// selecting all required elements 
+const editableInput = document.querySelector(".editable"),
+readonlyInput = document.querySelector(".readonly"),
+placeholder = document.querySelector(".placeholder"),
+counter = document.querySelector(".counter"),
+button = document.querySelector("button");
+
+editableInput.onkeyup = (e) => {
+  let element = e.target;
+  let maxLength = 5;
+  let currentLength = element.innerText.length;
+  let textTag = element.innerHTML;
+
+  if (currentLength <= 0) {
+    placeholder.style.display = "block";
+    counter.style.display = "none";
+    button.classList.remove("active");
+  } else {
+    counter.style.display = "block";
+    placeholder.style.display = "none";
+    button.classList.add("active");
+  };
+
+  counter.innerText = maxLength - currentLength;
+
+  if (currentLength > maxLength) {
+    let overText = element.innerText.substr(maxLength);
+    // subst() method extracts parts of a string, beginning at the character at the specified position, and returns the specified number of characters.
+    overText = `<span class="highlight">${overText}</span>`; // creating new span and passing over texts
+    textTag = element.innerText.substr(0, maxLength) + overText;
+    readonlyInput.style.zIndex = "1";
+    counter.style.color = "#e0245e";
+
+
+  };
+  readonlyInput.innerHTML = textTag;
+}
+
+
+
